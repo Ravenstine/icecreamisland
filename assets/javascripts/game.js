0fdef972ce6fa8c95eb94e49193f893141750305
benjamin <benjamin@benjamin-1005HA.(none)>
2014-01-04 17:25:53 -0800

    Work functions of table cells.

--- assets/javascripts/game.js
+++ assets/javascripts/game.js
@@ -1,9 +1,45 @@
 var Board = {
 
   init: function(){
-    $('form#new_item').on('ajax:success', this.handleItem)
+    var boardTable = $('table')
+    this.generateBoardArray()
+    this.boardTableArray()
+    this.populateBoardTable()
+    
+    $('#game_board').on('click', 'td', function(){
+    	$(this).toggleClass('current')
+    })
+
+  },
+
+  generateBoardArray: function(){
+  	this.boardArray = [[0,2,1,1,1,0],[0,1,2,2,1,2],[0,2,1,2,1,1],[0,0,2,1,1,2],[1,1,1,1,2,1],[2,2,2,1,2,1]]
+  },
+
+  populateBoardTable: function(){
+  	var self = this
+  	simpleBoardArray = []
+  	var simpleBoardArray = [];
+	simpleBoardArray = simpleBoardArray.concat.apply(simpleBoardArray, self.boardArray)
+  	$(simpleBoardArray).each(function(cellIndex, cell){
+	  	$($('td')[cellIndex]).addClass("basic" + cell.toString())
+  	})
+
+  },
+
+  boardTableArray: function(){
+  	var self = this
+  	outputArray = []
+  	$('table tr').each(function(rowIndex, row){
+  		var rowArray = []
+  		$($(row).find('td')).each(function(cellIndex, cell){
+  			rowArray.push(cell)
+  		})
+  		outputArray.push(rowArray)
+  	})
+  	  this.tableArray = outputArray
   }
-  
+
 }
 
 $(document).ready(function(){
